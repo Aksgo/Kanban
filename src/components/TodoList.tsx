ef6f107bb3de673bff4d73f247ebe832e6624188
Aksgo <akshitgoel0412@gmail.com>
2025-05-30 11:05:21 +0530

    empty containers handled

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,6 +3,7 @@ import "../output.css";
 import { Todo } from "../model";
 import SingleTodo from "./SingleTodo";
 import CompleteTodo from "./CompleteTodo";
+import { SiTicktick } from "react-icons/si";
 interface Props {
     todos:Todo[];
     setTodos:React.Dispatch<React.SetStateAction<Todo[]>>;
@@ -12,30 +13,30 @@ const TodoList : React.FC<Props> = ({todos, setTodos})=>{
         <div className="todos-board flex m-10 justify-center">
             <div className=" todos  flex-1 relative text-gray-300 bg-gray-900 flex flex-col m-10 items-center rounded-md max-w-1/3">
                 <header className="top-0 absolute font-medium text-xl bg-pink-800 p-2 rounded-sm w-full text-center">Assigned</header> 
-                <div className="mt-11 flex flex-col w-full">
-                {
+                <div className="mt-11 flex flex-col w-full h-90 overflow-y-auto">
+                {   todos.filter((todo)=>(todo.isDone===false)).length>0?
                     todos.map(todo => (
                         <SingleTodo
                         todo = {todo}
                         todos = {todos}
                         setTodos = {setTodos}
                         />
-                    ))
+                    )):(<span className="text-center mt-45 text-gray-400 text-2xl flex gap-3 items-center justify-center">No Tasks<SiTicktick/></span>)
                 }
                 </div>  
             </div>
 
             <div className=" todos relative flex-1 text-gray-300 bg-gray-900 flex flex-col m-10 items-center rounded-md max-w-1/3">
                 <header className="top-0 absolute font-medium text-xl bg-green-700 p-2 rounded-sm w-full text-center">Completed</header> 
-                <div className="mt-11 flex flex-col w-full">
-                {
+                <div className="mt-11 flex flex-col w-full h-90 overflow-y-auto">
+                {   todos.filter((todo)=>(todo.isDone===true)).length>0?
                     todos.map(todo => (
                         <CompleteTodo
                         todo = {todo}
                         todos = {todos}
                         setTodos = {setTodos}
                         />
-                    ))
+                    )):(<span className="text-center mt-45 text-gray-400 text-2xl flex gap-3 items-center justify-center">Nothing to Show ...<SiTicktick/></span>)
                 }
                 </div>  
             </div>
